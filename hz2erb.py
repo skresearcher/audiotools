273364d66014e4607a01a8e4cb8910d3d7606082
Shashaank N <shashaank04@gmail.com>
2020-05-30 12:27:38 -0400

    Add hz2erb.py conversion tool

---
+++ hz2erb.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# MATLAB's function converts from frequency in Hz to frequency in ERB
+def hz2erbMATLAB(hz):
+    return ((1000 * np.log(10)) / (24.7 * 4.37)) * np.log10((1 + hz * 0.00437))
+
+# Slaney's function converts from frequency in Hz to bandwith in ERB to determine the optimal bandwith of the gammatone filter.
+def hz2erbSlaney(hz):
+    EarQ = 9.26449
+    minBW = 24.7
+    order = 1
+    return ((hz / EarQ) ** order + minBW ** order) ** (1 / order)
+    
+# Glasberg and Moore's function is used my MATLAB in their gammatone filterbank to convert from frequency in Hz to bandwith in ERB.
+# https://www.mathworks.com/help/audio/ref/gammatonefilterbank-system-object.html
+def hz2erbGlasberg(hz):
+    return 24.7 * (4.37 * hz / 1000 + 1)
+
+# Slaney's function and Glasberg's function return nearly identical values. Slaney's function's decimal place accuracy is more than Glasberg's function
+print ("MATLAB: ", hz2erbMATLAB(440))
+print ("Slaney: ", hz2erbSlaney(440))
+print ("Glasberg and Moore: ", hz2erbGlasberg(440))
