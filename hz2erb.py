cb3087538f189f304140ef67d39c0373262e3a25
Shashaank N <shashaank04@gmail.com>
2020-05-30 12:30:07 -0400

    Update hz2erb.py

--- hz2erb.py
+++ hz2erb.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# MATLAB's function converts from frequency in Hz to frequency in ERB
+# MATLAB's function converts from frequency in Hz to frequency in ERB.
 def hz2erbMATLAB(hz):
     return ((1000 * np.log(10)) / (24.7 * 4.37)) * np.log10((1 + hz * 0.00437))
 
@@ -16,7 +16,7 @@ def hz2erbSlaney(hz):
 def hz2erbGlasberg(hz):
     return 24.7 * (4.37 * hz / 1000 + 1)
 
-# Slaney's function and Glasberg's function return nearly identical values
+# Slaney's function and Glasberg's function return nearly identical values.
 # Slaney's function's decimal place accuracy is more than Glasberg's function.
 print ("MATLAB: ", hz2erbMATLAB(440))
 print ("Slaney: ", hz2erbSlaney(440))
